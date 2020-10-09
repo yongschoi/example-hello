a5798408706c68dc5b7f8c21f89bf29d9faf1afb
yongschoi <yongschoi@naver.com>
2020-10-09 15:27:50 +0900

    test

--- telegram_notification.sh
+++ telegram_notification.sh
@@ -1,3 +1,3 @@
 
 ALERT_TEXT = "Travis build : $TRAVIS_TEST_RESULT"
-curl -s -X POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage -d chat_id=$TELEGRAM_CHAT_ID -d text="$ALERT_TEXT" -d parse_mode="Markdown"
+curl -s -X POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage -d chat_id=$TELEGRAM_CHAT_ID -d text="${ALERT_TEXT}" -d parse_mode="Markdown"
