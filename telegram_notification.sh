4b24dd0927be01a1a38c14e3a1e820d197b9d848
yongschoi <yongschoi@naver.com>
2020-10-09 15:07:24 +0900

    test

--- telegram_notification.sh
+++ telegram_notification.sh
@@ -1,3 +1,3 @@
 
 
-curl -s -X POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage -d chat_id=$TELEGRAM_CHAT_ID -d text="Travis build" -d parse_mode="Markdown"
+curl -s -X POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage -d chat_id=$TELEGRAM_CHAT_ID -d text="result:$TRAVIS_TEST_RESULT | repository:${TRAVIS_REPO_SLUG} | branch:${TRAVIS_BRANCH} | job url:${TRAVIS_JOB_WEB_URL}" -d parse_mode="Markdown"
