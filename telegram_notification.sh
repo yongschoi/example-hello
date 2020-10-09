075dcbd9fa676ae047ebf52245e26a2a1d93dc8f
yongschoi <yongschoi@naver.com>
2020-10-09 15:04:31 +0900

    ㅅㄷㄴㅅ

--- telegram_notification.sh
+++ telegram_notification.sh
@@ -1,17 +1,3 @@
 
-if [ $TRAVIS_TEST_RESULT -ne 0 ]; then
-    build_status="failed"
-else
-    build_status="succeeded"
-fi
 
-curl -s -X POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage -d chat_id=$TELEGRAM_CHAT_ID -d text="
--------------------------------------
-Travis build *${build_status}!*
-\`Repository:  ${TRAVIS_REPO_SLUG}\`
-\`Branch:      ${TRAVIS_BRANCH}\`
-*Commit Msg:*
-${TRAVIS_COMMIT_MESSAGE}
-[Job Log here](${TRAVIS_JOB_WEB_URL})
---------------------------------------
-"
+curl -s -X POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage -d chat_id=$TELEGRAM_CHAT_ID -d text="Travis build" -d parse_mode="Markdown"
