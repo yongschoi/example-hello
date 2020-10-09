d04ae2c281e9cb1c5b8d73598e720d9b14c77c9b
yongschoi <yongschoi@naver.com>
2020-10-09 15:49:26 +0900

    정말 마지막

--- telegram_notification.sh
+++ telegram_notification.sh
@@ -2,21 +2,11 @@
 
 # Define send message function. parse_mode can be changed to
 # HTML, depending on how you want to format your message:
-send_msg () {
-    curl -s -X POST https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage -d chat_id=$TELEGRAM_CHAT_ID \
-        -d text="$1" -d parse_mode="Markdown"
-}
-
-# Send message to the bot with some pertinent details about the job
-# Note that for Markdown, you need to escape any backtick (inline-code)
-# characters, since they're reserved in bash
-send_msg "
--------------------------------------
+curl -s -X POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage -d chat_id=$TELEGRAM_CHAT_ID -d text="-------------------------------------
 Travis build *${TRAVIS_TEST_RESULT}*
 \`Repository:  ${TRAVIS_REPO_SLUG}\`
 \`Branch:      ${TRAVIS_BRANCH}\`
 *Commit Msg:*
 ${TRAVIS_COMMIT_MESSAGE}
 [Job Log here](${TRAVIS_JOB_WEB_URL})
---------------------------------------
-"
+--------------------------------------" -d parse_mode="Markdown"
