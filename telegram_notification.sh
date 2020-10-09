cb18094b01235b1f7cf4c33d51c8acc64190f7a5
yongschoi <yongschoi@naver.com>
2020-10-09 15:12:02 +0900

    test

--- telegram_notification.sh
+++ telegram_notification.sh
@@ -1,3 +1,13 @@
+# Use built-in Travis variables to check if all previous steps passed:
+if [ $TRAVIS_TEST_RESULT -ne 0 ]; then
+    build_status="failed"
+else
+    build_status="succeeded"
+fi
 
-
-curl -s -X POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage -d chat_id=$TELEGRAM_CHAT_ID -d text="result:$TRAVIS_TEST_RESULT | repository:${TRAVIS_REPO_SLUG} | branch:${TRAVIS_BRANCH} | job url:${TRAVIS_JOB_WEB_URL}" -d parse_mode="Markdown"
+curl -s -X POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage -d chat_id=$TELEGRAM_CHAT_ID -d text="Travis build *${build_status}!*
+\`Repository:  ${TRAVIS_REPO_SLUG}\`
+\`Branch:      ${TRAVIS_BRANCH}\`
+*Commit Msg:*
+${TRAVIS_COMMIT_MESSAGE}
+[Job Log here](${TRAVIS_JOB_WEB_URL})" -d parse_mode="Markdown"
