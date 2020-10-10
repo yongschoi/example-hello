4d75318a105335abd5ee544799ddab7e37d5f738
yongschoi <yongschoi@naver.com>
2020-10-10 21:07:28 +0900

    v2 두번째 테스트입니다.

--- src/test/java/yongs/temp/UserServiceTest.java
+++ src/test/java/yongs/temp/UserServiceTest.java
@@ -27,8 +27,7 @@ class UserServiceTest {
 		// Junit은 Autowired를 통해 new가 되지 않음
 		UserService userService = new UserService();
 		   
-		// int expectedSize = 8;
-		int expectedSize = 7;
+		int expectedSize = 8;
 		
 		List<User> list = userService.findByScoreRatingExcludeZero();
 		assertEquals(expectedSize, list.size());	
