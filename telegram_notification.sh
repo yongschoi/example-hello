b6726797dbb5137feee30df48f96897c3827aa3e
yongschoi <yongschoi@naver.com>
2020-10-10 09:17:06 +0900

    test

--- telegram_notification.sh
+++ telegram_notification.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-if [ ${TRAVIS_TEST_RESULT} -ne 0 ]; then
-    build_status="Failed"
-else
-    build_status="Succeeded"
-fi
 
 # Define send message function. parse_mode can be changed to
 # HTML, depending on how you want to format your message:
@@ -11,10 +6,11 @@ curl -s -X POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage \
 -d chat_id=$TELEGRAM_CHAT_ID \
 -d text="
 -------------------------------------
-Travis build *${build_status}*
+Travis build *${TRAVIS_TEST_RESULT}*
 \`Repository: ${TRAVIS_REPO_SLUG}\`
 \`Branch: ${TRAVIS_BRANCH}\`
 *Commit Msg:*
 ${TRAVIS_COMMIT_MESSAGE}
 [Job Log here](${TRAVIS_JOB_WEB_URL})
---------------------------------------"
+--------------------------------------" \
+-d parse_mode="Markdown"
