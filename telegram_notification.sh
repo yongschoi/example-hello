a1466b991150c5f0a99585063bd07512b18f89dc
yongschoi <yongschoi@naver.com>
2020-10-10 09:13:33 +0900

    test

--- telegram_notification.sh
+++ telegram_notification.sh
@@ -7,7 +7,9 @@ fi
 
 # Define send message function. parse_mode can be changed to
 # HTML, depending on how you want to format your message:
-curl -s -X POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage -d chat_id=$TELEGRAM_CHAT_ID -d text="
+curl -s -X POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage \
+-d chat_id=$TELEGRAM_CHAT_ID \
+-d text="
 -------------------------------------
 Travis build *${build_status}*
 \`Repository: ${TRAVIS_REPO_SLUG}\`
@@ -15,4 +17,4 @@ Travis build *${build_status}*
 *Commit Msg:*
 ${TRAVIS_COMMIT_MESSAGE}
 [Job Log here](${TRAVIS_JOB_WEB_URL})
---------------------------------------" -d parse_mode="Markdown"
+--------------------------------------"
